0b668c4aaa9cc9cbff9555ed8ab147de62b3fa90
kylesung0520 <kylesung0520@gmail.com>
2023-09-26 17:43:06 +0900

    09/26 Q1

--- JavaTest/src/Sep26_Q1.java
+++ JavaTest/src/Sep26_Q1.java
@@ -1,5 +1,52 @@
+/*
+* 백준 2108 - 통계학 (구현)
+* */
+
+import java.util.*;
+import java.io.*;
 public class Sep26_Q1 {
-    public static void main(String[] args){
-        System.out.println("Hello");
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        Map<Integer, Integer> hs = new HashMap<>();
+        int sum = 0, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < N; i++){
+            int num = Integer.parseInt(br.readLine());
+            sum += num;
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+
+            arr[i] = num;
+
+            hs.put(num, hs.getOrDefault(num, 0)+1);
+        }
+        List<Integer> num = new ArrayList<>();
+        int tmp = Integer.MIN_VALUE;
+        for(Integer each : hs.keySet()){
+            if(hs.get(each) > tmp){
+                num = new ArrayList<>();
+                num.add(each);
+                tmp = hs.get(each);
+            }
+            else if(hs.get(each) == tmp){
+                num.add(each);
+            }
+        }
+        Arrays.sort(arr);
+        Collections.sort(num);
+        int idx = num.size() == 1 ? 0 : 1;
+        for(Integer each : num){
+            tmp = each;
+            idx--;
+            if(idx < 0){
+                break;
+            }
+        }
+        int average = (int) Math.round((double) sum / (double) arr.length);
+        sb.append(average+"\n").append(arr[arr.length/2]+"\n").append(tmp+"\n").append(max-min);
+        System.out.print(sb);
     }
 }
