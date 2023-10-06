f244562779dd4342986fa1bb8ef55b34b2b96c31
kylesung0520 <kylesung0520@gmail.com>
2023-10-06 15:30:26 +0900

    10/06 누적합 연습

---
+++ JavaTest/src/Oct/Oct06_Q2.java
@@ -0,0 +1,34 @@
+package Oct;
+import java.util.*;
+import java.io.*;
+public class Oct06_Q2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] prefix = new int[N+1];
+        int sum = 0;
+        for(int i = 1; i <= N; i++){
+            sum += Integer.parseInt(st.nextToken());
+            prefix[i] = sum;
+        }
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            int ret = prefix[end] - (prefix[start-1]);
+
+            sb.append(ret).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
